503703664322e39381cafb9c591c4e6fc23809aa
Aarifa19 <lebogangmogano849@gmail.com>
2024-12-11 12:42:32 +0200

    removed weather HTML details

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity-element");
   let windElement = document.querySelector("#wind-element");
   let iconElement = document.querySelector("#icon-element");
+  let time = document.querySelector("#time");
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
@@ -13,6 +14,7 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" />`;
+  console.log(response.data.time);
 }
 
 function search(event) {
@@ -117,3 +119,6 @@ function searchPolokwane() {
   axios.get(apiUrl).then(displayTemperature);
 }
 searchPolokwane();
+
+//#3b4a6b
+//#f65282 //pink
